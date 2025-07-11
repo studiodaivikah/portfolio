2a2319d4fb4877d7bcfe5be4a2950fcb81d47809
Mohamed-Parvez <clash.parwesh@gmail.com>
2025-07-12 00:22:47 +0530

    styled admin page

--- components/admin/admin.tsx
+++ components/admin/admin.tsx
@@ -7,26 +7,26 @@ const Admin = () => {
   const [currentpage, setCurrentpage] = useState("");
   return (
     <main className="flex flex-col items-center max-w-[2500px] w-full">
-      <main className="flex-center bg-black h-[66px] mx-auto max-w-[2800px] gap-5 w-full">
-        <div className="max-w-[1240px] flex-between w-full px-10">
+      <main className="flex-center bg-black h-[66px] mx-auto gap-5 w-full">
+        <div className="max-w-[1140px] flex-between w-full px-10">
           <p className="text-[20px] font-normal text-white">Admin Panel</p>
           <div className="flex-center gap-5">
             <button
               onClick={() => setCurrentpage("portfolio")}
-              className="text-[16px] font-normal px-4 py-2 cursor-pointer border border-slate-300 rounded-full text-white"
+              className="text-[14px] font-normal px-4 py-2 cursor-pointer border border-slate-300 rounded-full text-white"
             >
               Edit Portfolio
             </button>
             <button
               onClick={() => setCurrentpage("news")}
-              className="text-[16px] border cursor-pointer border-slate-300 rounded-full px-4 py-2 font-normal text-white"
+              className="text-[14px] border cursor-pointer border-slate-300 rounded-full px-4 py-2 font-normal text-white"
             >
               Edit News
             </button>
           </div>
         </div>
       </main>
-      <main className="max-w-[1440px] w-full flex-center">
+      <main className="max-w-[1180px] px-10 my-4 w-full flex-center">
         {currentpage === "portfolio" ? (
           <PortfolioManager />
         ) : currentpage === "news" ? (
--- components/admin/news/adminnews.tsx
+++ components/admin/news/adminnews.tsx
@@ -299,7 +299,7 @@ const AdminNews: React.FC = () => {
         </div>
       )}
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 mt-16 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {isLoading ? (
           <div className="col-span-full text-center py-8">
             <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto"></div>
