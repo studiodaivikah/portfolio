b10df13439df762d29a1f537a567edb78b957c5d
Parvez-Clutch <clash.parwesh@gmail.com>
2026-08-10 17:29:49 +0530

    feat: update branding assets and configure favicon and app icons in layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,13 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "/",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.png", type: "image/png", sizes: "512x512" },
+    ],
+    apple: [{ url: "/apple-icon.png", sizes: "180x180", type: "image/png" }],
+  },
   robots: {
     index: true,
     follow: true,
